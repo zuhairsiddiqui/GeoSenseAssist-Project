75267ca2b941620f49ba8aac82f4a38454c2da98
Sangam Bartaula <114772684+sangambartaula@users.noreply.github.com>
2025-03-07 19:17:42 -0600

    Delete website directory
    (Don't have git setup yet on VS Code, so going to replace my local files with the old ones.)

--- website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint('views', __name__)
-
-@views.route('/') # will run for main page
-def home():
-  return render_template("website.html")
