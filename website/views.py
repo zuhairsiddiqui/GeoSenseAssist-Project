089e56b94ad78ff358869250b4f1ce3b9c578673
Sangam Bartaula <114772684+sangambartaula@users.noreply.github.com>
2025-03-08 16:06:43 -0600

    Made upload file work and send to local machine
    Didn't push this change on last commit for some reason

--- website/views.py
+++ website/views.py
@@ -1,4 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
+# built-in python library installed when you install python. it helps make sure files uploaded are safe.
+import os
+# https://www.youtube.com/watch?v=GQLRVhXnZkE
+
+app = Flask(__name__)
+app.config['UPLOAD_DIRECTORY'] = 'uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB
 
 views = Blueprint('views', __name__)
 
@@ -14,6 +23,19 @@ def login():
 def logout():
   return render_template("logout.html")
 
-@views.route('sign-up')
+@views.route('/sign-up')
 def signup():
     return render_template("signup.html")
+
+@views.route('/upload', methods=['POST'])
+def upload():
+    try:
+        file = request.files['fileUpload']
+
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_DIRECTORY'], secure_filename(file.filename)))
+            
+    except RequestEntityTooLarge:
+        return "File is larger than the 16 MB Limit"
+
+    return redirect('/')
