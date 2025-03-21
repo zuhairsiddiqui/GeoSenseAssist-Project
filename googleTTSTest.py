63b780b3e5420a21289390f7861149760291b77b
drew-daffern <drewdaffern04@gmail.com>
2025-03-20 21:13:50 -0500

    Added comments and initial import

--- googleTTSTest.py
+++ googleTTSTest.py
@@ -1,1 +1,3 @@
 #initial testing of google TTS will go here
+#functions that read out an input string using tts will be created in this test file, and will then be used in the website 
+from gtts import gTTS
