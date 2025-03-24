e7404cd16668f96417d996873305a60630921e13
tanvibbsr <tanvibbsr@gmail.com>
2025-03-24 02:02:44 -0500

    Added some lines in textToSpeech.py

--- textToSpeech.py
+++ textToSpeech.py
@@ -5,3 +5,15 @@ engine = pyttsx3.init()
 def textToSpeech(text):
     engine.say(text)
     engine.runAndWait()
+
+#Example
+textToSpeech("Hello, how are you?")
+textToSpeech("This is a text-to-speech example.")
+
+# Set the speech rate (speed)
+#Slow speeds (50-100 ):
+#Moderate speeds (150-200 ):
+#Fast speeds (250-300 ): 
+#Very fast speeds (400+ )
+engine.setProperty('rate', 200)
+
