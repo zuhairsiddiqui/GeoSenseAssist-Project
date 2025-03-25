853cf0d8a4c82cf0508dc002380c3708bbd5cf3a
Andreas Paramo <andreasparamo@gmail.com>
2025-03-25 10:39:51 -0500

    modified entries.py to access from .env for ease of use

--- entries.py
+++ entries.py
@@ -1,13 +1,21 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
-# Connect to MySQL
+HOST_NAME = os.getenv("HOST_NAME")
+USER_NAME = os.getenv("USER_NAME")
+USER_PASSWORD = os.getenv("USER_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="aap0259",
-    password="Ap4699992142",
-    database="GeoSensedb"
-)
+        host=HOST_NAME,
+        user = USER_NAME,
+        password = USER_PASSWORD,
+        database=DATABASE_NAME
+    )
 
 cursor = mydb.cursor()
 
