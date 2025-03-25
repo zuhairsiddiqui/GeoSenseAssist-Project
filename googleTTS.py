5ad2b0f4998ee7d5ccd78e10129f9f24e5e38186
drew-daffern <drewdaffern04@gmail.com>
2025-03-25 15:37:01 -0500

    Removed import used for testing

--- googleTTS.py
+++ googleTTS.py
@@ -1,6 +1,5 @@
 #functions that read out an input string using tts will be created in this test file, and will then be used in the website 
 from gtts import gTTS
-import os
 
 def textToSpeech(userText):
     obj = gTTS(text=userText, lang='en', slow=False)
