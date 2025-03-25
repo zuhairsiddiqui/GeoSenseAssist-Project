957510365a27b63926d47b88a5533d7258a0609c
drew-daffern <drewdaffern04@gmail.com>
2025-03-25 15:35:25 -0500

    Created function to convert given string to .mp3 file

--- googleTTS.py
+++ googleTTS.py
@@ -1,3 +1,9 @@
-#initial testing of google TTS will go here
 #functions that read out an input string using tts will be created in this test file, and will then be used in the website 
 from gtts import gTTS
+import os
+
+def textToSpeech(userText):
+    obj = gTTS(text=userText, lang='en', slow=False)
+    obj.save("audio.mp3")
+
+
