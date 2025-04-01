a6319f22e86452753a4cb0e6b99f6f5cb3eba077
zuhairsiddiqui <zuhairjsiddiqui@gmail.com>
2025-04-01 12:10:42 -0500

    Pushing code to test CI feature

---
+++ tests/test_code.py
@@ -0,0 +1,14 @@
+import subprocess
+import time
+import requests
+
+# Start Flask server before tests
+flask_process = subprocess.Popen(["python", "main.py"])
+time.sleep(2)  # Give server time to start
+
+def test_api_status():
+    response = requests.get("http://127.0.0.1:5000")
+    assert response.status_code == 200
+
+# Stop server after tests
+flask_process.terminate()
--- website/views.py
+++ website/views.py
@@ -1,14 +1,35 @@
 from flask import Flask, Blueprint, render_template, request, redirect, send_from_directory, url_for
+import mysql.connector
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 # built-in python library installed when you install python. it helps make sure files uploaded are safe.
 import os
+import sys
 import importlib.util
 from . import buttonsFunctionality
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+import googleTTS
+
+
 from dotenv import load_dotenv
+import pygame
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
+
+HOST_NAME = os.getenv("HOST_NAME")
+USER_NAME = os.getenv("USER_NAME")
+USER_PASSWORD = os.getenv("USER_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+conn = mysql.connector.connect(
+        host=HOST_NAME,
+        user = USER_NAME,
+        password = USER_PASSWORD,
+        database=DATABASE_NAME
+  )
+
+cursor = conn.cursor()
 
 module_name = "ImageDetection"
 #module_path = "GeoSenseAssist-Project/ImageDetection.py"  # Adjust as needed
@@ -30,7 +51,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/') # will run for main page
 def home():
-  return render_template("website.html")
+  return render_template("website.html"), 200
 
 @views.route('/login')
 def login():
@@ -63,3 +84,13 @@ def upload_equation():
   equation, analysis = buttonsFunctionality.upload_equation("of a 9th - 12th grader")
   return render_template('equations.html', filename=equation, result=analysis)
 
+@views.route('/audio_analysis', methods=['POST'])
+def audio_analysis():
+   result = request.form.get("result")
+   filename = request.form.get("file")
+   shape = request.form.get("shape")
+   audio = googleTTS.textToSpeech(result,filename)
+   pygame.mixer.init()
+   pygame.mixer.music.load(audio)
+   pygame.mixer.music.play()
+   return render_template('Shapes.html', filename=shape, result=result)
