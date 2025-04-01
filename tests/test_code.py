54faa175f198a4faeb3e6f1031ba6e99f5762b8a
Zuhair Siddiqui <zuhairjsiddiqui@gmail.com>
2025-04-01 15:01:35 -0500

    trying a new method on test_code.py to make it work for CI integration

--- tests/test_code.py
+++ tests/test_code.py
@@ -2,13 +2,12 @@ import subprocess
 import time
 import requests
 
-# Start Flask server before tests
-flask_process = subprocess.Popen(["python", "main.py"])
-time.sleep(10)  # Give server time to start
-
 def test_api_status():
     try:
-        response = requests.get("http://localhost:5000")
+        app = create_app()
+        time.sleep(10)
+        client = app.test_client()
+        response = client.get('/')
         assert response.status_code == 200
     except requests.exceptions.ConnectionError as e:
         print("Connection Error:", e)
